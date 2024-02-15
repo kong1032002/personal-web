df0899f4d418c999998789fc915fbe2e28d2115b
Thanh Cong Nguyen <cong.nguyenthanh@devfast.us>
2024-02-15 14:18:00 +0700

    test api

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,26 @@
-<script lang="ts" setup></script>
-
 <template>
   <div>
     Page: foo
   </div>
+  <pre>
+    {{ data }}
+  </pre>
 </template>
 
 <style scoped></style>
+
+<script lang="ts" setup>
+
+const data = await useFetchClient().fetchClientFactory.POST('https://api.sticpay.com/rest_pay/pay',
+  {
+    "merchant_email": "greenpotserviceslimited@gmail.com",
+    "order_no": "12345abcde",
+    "order_time": "2021-10-20 11:14:23",
+    "order_amount": "10.23",
+    "order_currency": "USD",
+    "sign": "b1008105b9e203b64cb9a4691095f13c",
+    "interface_version": "sandbox"
+  }
+)
+
+</script>
